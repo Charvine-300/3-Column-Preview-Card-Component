448f5129f186e433c87a1cbaebd0a50e4da1f108
LordEra <marvinezenwekwe@gmail.com>
2021-06-22 23:49:11 +0100

    DannyBello

--- card.js
+++ card.js
@@ -1,1 +1,20 @@
-console.log("Line in the sand");
+function startUp() {
+    var button = document.getElementById("learn");
+    console.log(button);
+
+    button.onmousedown = function() {
+        button.style.width = "145px";
+    }
+    button.onmouseup = function() {
+        button.style.width = "150px";
+    }
+}
+
+
+
+window.onload = function() {
+    startUp();
+}
+
+
+
